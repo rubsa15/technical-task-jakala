0e7dd7dde40a22bb76b6e12a3bc1f8d55cc57256
Ruben Sanz <ruben.sanz@familiados.com>
2025-01-26 09:37:32 +0100

    implement hooks

---
+++ src/hooks/useGetProductDetail.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+import { ProductService } from '../services/product.service';
+import { Product } from '../domain/Product';
+
+export const useGetProductDetails = (productId: string | undefined) => {
+  const [productDetail, setProductDetail] = useState<Product | undefined>(
+    undefined
+  );
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    if (productId) {
+      getProductDetail(productId);
+    }
+  }, [productId]);
+
+  const getProductDetail = async (id: string) => {
+    try {
+      setIsLoading(true);
+      const product = await ProductService.getDetail(id);
+      setProductDetail(product);
+    } catch (error) {
+      console.log(error);
+    }
+    setIsLoading(false);
+  };
+
+  return { productDetail, isLoading };
+};
